34950d44e16449f20277550b350320ae50fc3baf
Sushovan Majhi <sushovan.majhi08@gmail.com>
2020-01-16 10:22:59 -0600

    update

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,6 @@
-(function( ){
-    
-})( );
+$(document).ready(function( ) {
+    $('block').each(function() {
+	$( this ).prepend('<div class="header"></div>');
+	$( this ).find('.header').html($( this ).data('header'));
+    });
+});
