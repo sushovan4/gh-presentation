1788cc97e8a68bc00fca291423b15c7cfc8766d8
Sushovan Majhi <sushovan.majhi08@gmail.com>
2020-01-16 20:56:58 -0600

    add

--- js/custom.js
+++ js/custom.js
@@ -3,4 +3,9 @@ $(document).ready(function( ) {
 	$( this ).prepend('<div class="header"></div>');
 	$( this ).find('.header').html($( this ).data('header'));
     });
+    $('definition').each(function() {
+	$( this ).prepend('<div class="header">Definition</div>');
+	$( this ).find('.header').append(' (' + $( this ).data('header') + ')');
+    });
+
 });
